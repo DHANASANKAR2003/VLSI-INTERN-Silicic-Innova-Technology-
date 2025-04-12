f56b7173c3c0437e5632489cc15857ed974e0cac
DHANASANKAR2003 <146157469+DHANASANKAR2003@users.noreply.github.com>
2025-04-12 12:17:01 +0530

    Update JK_ff.v

--- COMBINATIONAL/DAY 3/JK_ff.v
+++ COMBINATIONAL/DAY 3/JK_ff.v
@@ -11,10 +11,10 @@ module jk_ff(
     else 
       begin
         case({j,k})
-          2'b00:q <= 1'bx;
+          2'b00:q <= q;
           2'b01:q <= 0;
           2'b10:q <= 1;
-          2'b11:q <= q;
+          2'b11:q <= ~q;
         endcase
       end
   end
