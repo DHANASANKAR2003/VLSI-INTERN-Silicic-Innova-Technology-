4b1200bb784d5b75fea69931df0c17ca26385be4
DHANASANKAR2003 <146157469+DHANASANKAR2003@users.noreply.github.com>
2025-04-17 13:54:09 +0530

    Update 03_ConwaysGameofLife16x16.v

--- HDL_Bits/2. Circuits/2. Sequential Logic/4. More Circuits/03_ConwaysGameofLife16x16.v
+++ HDL_Bits/2. Circuits/2. Sequential Logic/4. More Circuits/03_ConwaysGameofLife16x16.v
@@ -1,63 +1,51 @@
+
 module top_module(
     input clk,
     input load,
     input [255:0] data,
     output [255:0] q ); 
-     wire [2:0] pop;
-    assign pop = {2'b00, neigh[0]} +
-                 {2'b00, neigh[1]} +
-                 {2'b00, neigh[2]} +
-                 {2'b00, neigh[3]} +
-                 {2'b00, neigh[4]} +
-                 {2'b00, neigh[5]} +
-                 {2'b00, neigh[6]} +
-                 {2'b00, neigh[7]}; 
     
-    reg tmp;
-    assign next = tmp;
-
+    reg [255:0] q_next;
+    integer i, j;
+    integer i_s, i_a, j_s, j_a;
+    
     always @(*) begin
-        case (pop)
-            2: tmp = current;
-            3: tmp = 1;
-            default: tmp = 0;
-        endcase
+        for(i = 0; i < 16; ++ i)
+            for(j = 0; j < 16; ++ j)
+                begin
+                    if(i == 0)
+                        i_s = 15;
+                    else
+                        i_s = i - 1;
+                    
+                    if(j == 0)
+                        j_s = 15;
+                    else
+                        j_s = j - 1;
+                    
+                    if(i == 15)
+                        i_a = 0;
+                    else
+                        i_a = i + 1;
+                    
+                    if(j == 15)
+                        j_a = 0;
+                    else
+                        j_a = j + 1;
+                    
+                    case(q[i_a+j_s*16] + q[i_s+j_a*16] + q[i_s+j_s*16] + q[i_a+j_a*16] + q[i_s+j*16] + q[i_a+j*16] + q[i+j_s*16] + q[i+j_a*16])
+                        4'b0010: q_next[i+j*16] <= q[i+j*16];
+                        4'b0011: q_next[i+j*16] <= 1;
+                        default: q_next[i+j*16] <= 0;
+                    endcase
+                end
     end
-endmodule
-
-module top_module(
-    input clk,
-    input load,
-    input [255:0] data,
-    output [255:0] q ); 
-	
-    wire [255:0] next;
-    
-    genvar x, y;
-    generate
-        for (x=0; x<=15; x=x+1) begin : gen_x
-            for (y=0; y<=15; y=y+1) begin : gen_y
-                rule fate (
-                    .neigh({q[(x==0 ? 15 : x-1) + (y==0 ? 15 : y-1)*16],q[(x==0 ? 15 : x-1) + y                *16],
-                            q[(x==0 ? 15 : x-1) + (y==15 ? 0 : y+1)*16],
-                            q[x                 + (y==0 ? 15 : y-1)*16],
-                            q[x                 + (y==15 ? 0 : y+1)*16],
-                            q[(x==15 ? 0 : x+1) + (y==0 ? 15 : y-1)*16],
-                            q[(x==15 ? 0 : x+1) + y                *16],
-                            q[(x==15 ? 0 : x+1) + (y==15 ? 0 : y+1)*16]}),
-                    .current(q[(x + y*16)]),
-                    .next(next[(x + y*16)])
-                );
-            end
-        end
-    endgenerate
     
     always @(posedge clk) begin
-        if (load) begin
+        if(load)
             q <= data;
-        end else begin
-            q <= next;
-        end
+        else
+	    	q <= q_next;
     end
 
 endmodule
